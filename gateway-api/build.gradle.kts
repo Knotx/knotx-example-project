5f6d0bf0c9ed9744de9ddc584cadb83bd5822e1e
Piotr Marcinkowski <piotr.marcinkowski@cognifide.com>
2019-06-10 10:11:34 +0200

    empty chars remove

--- gateway-api/build.gradle.kts
+++ gateway-api/build.gradle.kts
@@ -5,11 +5,9 @@ plugins {
 
 dependencies {
     implementation(platform("io.knotx:knotx-dependencies:${project.version}"))
-
     implementation("io.knotx:knotx-server-http-api:${project.version}")
     implementation("io.knotx:knotx-fragment-api:${project.version}")
     implementation("io.knotx:knotx-fragments-handler-api:${project.version}")
-
     implementation(group = "org.apache.commons", name = "commons-lang3")
     implementation(group = "io.vertx", name = "vertx-web")
     implementation(group = "io.vertx", name = "vertx-web-client")
