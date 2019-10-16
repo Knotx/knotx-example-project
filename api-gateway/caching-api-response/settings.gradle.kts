d8641a8345a6e29617a0faaae2ac7bb9a9f8a141
Oskar Jerzyk <oskar.jerzyk@cognifide.com>
2019-10-16 16:06:22 +0200

    Caching HTTP requests tutorial created - demo code provided

---
+++ api-gateway/caching-api-response/settings.gradle.kts
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2019 Knot.x Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+rootProject.name = "api-cache"
+
+include("health-check")
+include("example-api")
+include("example-action")
+
+project(":health-check").projectDir = file("modules/health-check")
+project(":example-api").projectDir = file("modules/example-api")
+project(":example-action").projectDir = file("modules/example-action")
